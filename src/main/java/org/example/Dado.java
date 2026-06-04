0a3d55ad3cdf71a440a4749b86f592f206644510
dci <n.molfinqueo01@ufromail.cl>
2026-06-04 10:07:14 -0400

    Codigo Generado por Visual Paradigm

---
+++ src/main/java/org/example/Dado.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Dado {
+
+    private int caraSuperior;
+
+    public void lanzar() {
+        // TODO - implement Dado.lanzar
+        throw new UnsupportedOperationException();
+    }
+
+    public int getCaraSuperior() {
+        return this.caraSuperior;
+    }
+
+}
---
+++ src/main/java/org/example/JuegoDados.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class JuegoDados {
+
+    private Dado dado1;
+    private Dado dado2;
+    private Calculadora calculadora;
+
+    public boolean jugar() {
+        // TODO - implement JuegoDados.jugar
+        throw new UnsupportedOperationException();
+    }
+
+    public int obtenerSuma() {
+        // TODO - implement JuegoDados.obtenerSuma
+        throw new UnsupportedOperationException();
+    }
+
+}
